5b585da6d1b9c4a76bd3f4d60c8c92f63a058929
benmucha <43706062+benmucha@users.noreply.github.com>
2022-01-21 23:19:09 -0500

    Refactor main script

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -7,11 +7,11 @@ public class Main : MonoBehaviour
     public GameObject Player;
     List<GameObject> tiles = new List<GameObject>();
     public GameObject tile;
+
     // Start is called before the first frame update
     void Start()
     {
-
-        make_board();
+        MakeBoard();
     }
 
     // Update is called once per frame
@@ -20,25 +20,27 @@ public class Main : MonoBehaviour
         
     }
     
-    void make_board()
+    /// <summary>
+    /// Makes the game board.
+    /// </summary>
+    void MakeBoard()
     {
         for (int i = 0; i < 10; i++)
-        {
-            tiles.Add(GameObject.Instantiate(tile, new Vector3(i * 5, -.25f, 0), Quaternion.identity));
-        }
+            AddTile(new Vector3(i * 5, -.25f, 0));
         for (int i = 0; i < 8; i++)
-        {
-            tiles.Add(GameObject.Instantiate(tile, new Vector3(45, -.25f, 5 + i * 5), Quaternion.identity));
-        }
+            AddTile(new Vector3(45, -.25f, 5 + i * 5));
         for (int i = 0; i < 10; i++)
-        {
-            tiles.Add(GameObject.Instantiate(tile, new Vector3(45 - 5 * i, -.25f, 40), Quaternion.identity));
-        }
+            AddTile(new Vector3(45 - 5 * i, -.25f, 40));
         for (int i = 0; i < 8; i++)
-        {
-            tiles.Add(GameObject.Instantiate(tile, new Vector3(0, -.25f, 35 - i * 5), Quaternion.identity));
-        }
+            AddTile(new Vector3(0, -.25f, 35 - i * 5));
     }
-    
+    /// <summary>
+    /// Adds a tile at the specified world position.
+    /// </summary>
+    /// <param name="tilePos">World position of the Tile to add</param>
+    void AddTile(Vector3 tilePos)
+    {
+        tiles.Add(GameObject.Instantiate(tile, tilePos, Quaternion.identity));
+    }
 }
 
